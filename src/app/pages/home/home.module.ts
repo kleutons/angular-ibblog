b44e255d17ff0be493cf3183cc680b2696033b85
kleuton novais <106082564+kleutons@users.noreply.github.com>
2023-10-11 15:18:13 -0300

    feat: add big-card

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -3,12 +3,16 @@ import { CommonModule } from '@angular/common';
 import { HeroSecitonComponent } from './hero-seciton/hero-seciton.component';
 import { PageHome } from './home.component';
 import { RouterModule } from '@angular/router';
+import { BigCardComponent } from './cards/big-card/big-card.component';
+import { FeaturedCardComponent } from './cards/featured-card/featured-card.component';
 
 
 @NgModule({
   declarations: [
     PageHome,
-    HeroSecitonComponent
+    HeroSecitonComponent,
+    BigCardComponent,
+    FeaturedCardComponent
   ],
   exports: [  
     PageHome
