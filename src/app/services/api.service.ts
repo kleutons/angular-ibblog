7a895f98b55d41740fb9aaf806457460badf5ca9
kleuton novais <106082564+kleutons@users.noreply.github.com>
2023-10-18 14:28:27 -0300

    fix hero and menu

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable, BehaviorSubject} from 'rxjs';
 import { map } from 'rxjs/operators'
-import { ECategory, newsData } from '../types/newsData';
+import { newsData } from '../types/newsData';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment'
 
